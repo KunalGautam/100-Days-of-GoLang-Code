27c380db3f63dc0e1c9521283fa0c0da7ab9473f
Kunal Gautam <kunal@ikunal.in>
2021-09-13 09:38:15 +0530

    Day 1 - Assigning types to variable

---
+++ 01/06-types.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//Specifying type of string
+var a string = "String Type"
+
+func main() {
+	//specifying type of int, float and bool
+	var b int = 2
+	var c float64 = 9.0
+	var d bool = true
+
+	fmt.Println(a,b,c,d)
+
+}
