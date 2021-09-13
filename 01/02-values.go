b9d53b076f525babf19b4776e4c24713e21c765e
Kunal Gautam <kunal@ikunal.in>
2021-09-13 09:14:55 +0530

    Day 1 - Mathematics Operations

--- 01/02-values.go
+++ 01/02-values.go
@@ -6,4 +6,13 @@ func main() {
 	//Concatenate Strings
 	fmt.Println("Hello" + " " + "World" + "!")
 
+	// Mathematic Operations
+
+	fmt.Println("Sum of 1 and 2 is ", 1+2)
+	fmt.Println("Difference of 7 and 2 is ", 7-2)
+	fmt.Println("Multiplication of 2 and 6 is ", 2*6)
+	fmt.Println("division of 7 and 4 is ", 7/4)
+	fmt.Println("division of 7 and 4 is ", 7.0/4.0)
+	fmt.Println("Remainder from 7/4 is ", 7%4)
+
 }
