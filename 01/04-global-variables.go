83de6aacffd4b3ab52d18ef1af4c80750f6bebb7
Kunal Gautam <kunal@ikunal.in>
2021-09-13 09:31:34 +0530

    Day 1 - Global Variables

---
+++ 01/04-global-variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var a string
+var b int
+var c float64
+var d bool
+
+func main() {
+	a = "String Variable"
+	b = 2
+	c = 9.0
+	d = false
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+
+}
