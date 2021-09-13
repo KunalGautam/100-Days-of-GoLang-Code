7dca1e5f4acd3fc1730378d7b2f11e8997fade16
Kunal Gautam <kunal@ikunal.in>
2021-09-13 09:28:58 +0530

    Day 1 - Variables & Formatted 01-helloWorld.go

---
+++ 01/03-variables.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Init String Variable a
+	var a = "String Variable"
+	fmt.Println(a)
+	// Init Int Variable b
+	var b = 2
+	fmt.Println(b)
+	// Init float Variable c
+	var c = 9.0
+	fmt.Println(c)
+	// Init bool Variable d
+	var d = true
+	fmt.Println(d)
+}
