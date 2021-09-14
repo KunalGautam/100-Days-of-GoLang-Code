15366aa9ab3e35fd5b9d7106aaa725dcbfb388a3
Kunal Gautam <kunal@ikunal.in>
2021-09-14 08:21:27 +0530

    Day 2 - Time: Step2

--- 02/03-time-package.go
+++ 02/03-time-package.go
@@ -8,4 +8,13 @@ import (
 func main() {
 	// Step 1 : Now returns the current local time.
 	fmt.Println(time.Now())
+	// Step 2 : Get Current Month, Year, Week, Day, date, hour, minutes, seconds of current Local Time
+	fmt.Println(time.Now().Month())
+	fmt.Println(time.Now().Year())
+	fmt.Println(time.Now().Weekday())
+	fmt.Println(time.Now().Day())
+	fmt.Println(time.Now().Date())
+	fmt.Println(time.Now().Hour())
+	fmt.Println(time.Now().Minute())
+	fmt.Println(time.Now().Second())
 }
