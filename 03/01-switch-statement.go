a9ce66be50c6d6af2381b8c0c8b612bc7516e52d
Kunal Gautam <kunal@ikunal.in>
2021-09-15 08:12:22 +0530

    Day 3 - Switch Statement - Step 4

--- 03/01-switch-statement.go
+++ 03/01-switch-statement.go
@@ -32,4 +32,14 @@ func main() {
 		fmt.Println("It's a weekday")
 	}
 
+	// Step 4: Don't pass expression to switch
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
 }
