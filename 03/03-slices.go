56c2c13d81a0365b4024d482c573f3b3218966de
Kunal Gautam <kunal@ikunal.in>
2021-09-15 08:45:37 +0530

    Day 3 - Slices - Step 2

--- 03/03-slices.go
+++ 03/03-slices.go
@@ -7,4 +7,10 @@ func main() {
 	// make() is a special built-in function that is used to initialize slices, maps, and channels.
 	a := make([]int, 4)
 	fmt.Println(a)
+	// Step 2: Set values to slice
+	a[0] = 2
+	a[1] = 4
+	a[2] = 6
+	a[3] = 8
+	fmt.Println(a)
 }
