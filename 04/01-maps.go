cf6b956f67455bbf32e4a6de15803e2fbfd35345
Kunal Gautam <kunal@ikunal.in>
2021-09-16 07:20:46 +0530

    Day 4 - Maps - Step 1

---
+++ 04/01-maps.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Step 1: Init a map (A map is key pair value)
+	a := make(map[string]int)
+	a["age"] = 20
+	a["height"] = 170
+	fmt.Println(a)
+
+}
---
+++ 04/go.mod
@@ -0,0 +1,3 @@
+module github.com/KunalGautam/100-Days-of-GoLang-Code/04
+
+go 1.17
