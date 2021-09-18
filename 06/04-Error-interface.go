763a7275d87e61888e787303d78a36ec11214224
Kunal Gautam <kunal@ikunal.in>
2021-09-18 12:56:51 +0530

    Day 6 - error interface

---
+++ 06/04-Error-interface.go
@@ -0,0 +1,26 @@
+// Example copied from https://www.digitalocean.com/community/tutorials/creating-custom-errors-in-go
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type MyError struct{}
+
+func (m *MyError) Error() string {
+	return "boom"
+}
+
+func sayHello() (string, error) {
+	return "", &MyError{}
+}
+
+func main() {
+	s, err := sayHello()
+	if err != nil {
+		fmt.Println("unexpected error: err:", err)
+		os.Exit(1)
+	}
+	fmt.Println("The string:", s)
+}
