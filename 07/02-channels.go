ab030c0d974afd9834bb85f7a6af997b361d3f43
Kunal Gautam <kunal@ikunal.in>
2021-09-19 07:31:32 +0530

    Day 7 - Channels Step2

--- 07/02-channels.go
+++ 07/02-channels.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	// Step1: Create channel using make()
+	//Step1: Create channel using make()
 	ch1 := make(chan string)
 	go func() { ch1 <- "ping" }()
 	go func() { ch1 <- "pong" }()
 
 	fmt.Println(<-ch1)
 	fmt.Println(<-ch1)
+
+	// Step2: Channel buffering
+	ch2 := make(chan string, 2)
+
+	go func() { ch2 <- "ping" }()
+	go func() { ch2 <- "pong" }()
+
+	fmt.Println(<-ch2)
+	fmt.Println(<-ch2)
 
 }
