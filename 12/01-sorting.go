a4199cac94b4439b7328988bab6b6990ce212a2f
Kunal Gautam <kunal@ikunal.in>
2021-09-24 07:26:54 +0530

    Day 12: Step3: Check if sorted.

--- 12/01-sorting.go
+++ 12/01-sorting.go
@@ -18,4 +18,16 @@ func main() {
 	sort.Ints(ints)
 	fmt.Println(ints)
 
+	// Step3: check if sorted
+	output := sort.IntsAreSorted(ints)
+	fmt.Println(output)
+	ints2 := []int{8, 98, 4, 176, 5}
+	output2 := sort.IntsAreSorted(ints2)
+	fmt.Println(output2)
+	output3 := sort.StringsAreSorted(str)
+	fmt.Println(output3)
+	str2 := []string{"z", "c", "w", "a"}
+	output4 := sort.StringsAreSorted(str2)
+	fmt.Println(output4)
+
 }
