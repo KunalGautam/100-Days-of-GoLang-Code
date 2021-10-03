c33f328d014fd5a7897d3d4aef34eb55ffe25855
Kunal Gautam <kunal@ikunal.in>
2021-10-03 20:53:35 +0530

    Day 21: Quiz Part 1 from https://github.com/gophercises/quiz

---
+++ 20-30/21/main.go
@@ -0,0 +1,48 @@
+// https://github.com/gophercises/quiz
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type quiz struct {
+	ques string
+	ans  string
+}
+
+func main() {
+	readCsvFile, err := os.ReadFile("data.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	csvParse := csv.NewReader(strings.NewReader(string(readCsvFile)))
+	csvData, err := csvParse.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	quizzes := make([]quiz, len(csvData))
+
+	for i, line := range csvData {
+
+		quizzes[i] = quiz{ques: line[0], ans: line[1]}
+	}
+
+	score := 0
+
+	for i, q := range quizzes {
+		fmt.Printf("Quiz #%d: %s = \n", i+1, q.ques)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == q.ans {
+			score++
+		}
+	}
+
+	fmt.Println("Your Score is ", score)
+
+}
