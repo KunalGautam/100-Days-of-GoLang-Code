c91f6d16d46b13fc8b37d7110e3cc443cd3ecfeb
Kunal Gautam <kunal@ikunal.in>
2021-10-06 20:29:32 +0530

    Day 23: Email sending using template.

--- 20-30/24/main.go
+++ 20-30/24/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"html/template"
 	"log"
 	"net/smtp"
 )
 
 func main() {
-	var (
-		from       = "demo@lan.ikunal.in"
-		msg        = []byte("Message to be sent.")
-		recipients = []string{"mail@example.com"}
-	)
 	// func PlainAuth(identity, username, password, host string) Auth
 	// Usually identity should be the empty string, to act as username.
 	// Setup Plain Auth details
@@ -20,7 +17,27 @@ func main() {
 	// If done over non-secure connection
 	smtpAuth := smtp.CRAMMD5Auth("demo@lan.ikunal.in", "p4ssw0rd")
 
-	err := smtp.SendMail("lan.ikunal.in:25", smtpAuth, from, recipients, msg)
+	t, err := template.ParseFiles("email.tpl")
+	if err != nil {
+		log.Panicln(err)
+	}
+	var data string
+	buf := &bytes.Buffer{}
+	if err = t.Execute(buf, data); err != nil {
+		log.Panicln(err)
+	}
+
+	templateRendered := buf.String()
+	const MIME = "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+
+	var (
+		from       = "demo@lan.ikunal.in"
+		subject    = "Welcome Email"
+		msg        = []byte(subject + MIME + "\n" + templateRendered)
+		recipients = []string{"mail@example.com"}
+	)
+
+	err = smtp.SendMail("lan.ikunal.in:25", smtpAuth, from, recipients, msg)
 
 	if err != nil {
 		log.Panicln(err)
