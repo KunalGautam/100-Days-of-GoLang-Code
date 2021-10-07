1a3d6f4f3c3033cbd4ed15bb14d87c76bb1507c7
Kunal Gautam <kunal@ikunal.in>
2021-10-07 09:43:23 +0530

    Day 24: HTML Template Rendring, check if data provided or not.

--- 20-30/25/main.go
+++ 20-30/25/main.go
@@ -8,13 +8,18 @@ import (
 )
 
 type indexData struct {
-	// Capitalize First letter of variable to make it available globally, else will have error of is an unexported field of struct type
-	Title string
-	User  string
+	// Capitalize First letter of variable to make it available globally, else will have error of is an unexported field of struct type. The renderer package use the reflect package in order to get/set fields values the reflect package can only access public/exported struct fields.
+	Title    string
+	User     string
+	Password string
 }
 
 func main() {
 	t, err := template.ParseFiles("template/index.html")
+	if err != nil {
+		log.Panicln(err)
+	}
+	t2, err := template.ParseFiles("template/index2.html")
 	if err != nil {
 		log.Panicln(err)
 	}
@@ -28,5 +33,18 @@ func main() {
 		log.Panicln(err)
 	}
 	renderedOutput := buffer.String()
+	fmt.Println(renderedOutput)
+
+	data = indexData{
+		Title: "Title of the Page2",
+		User:  "User2",
+		// Enable or disable if required.
+		//Password: "p455w0rd",
+	}
+	err = t2.Execute(buffer, data)
+	if err != nil {
+		log.Panicln(err)
+	}
+	renderedOutput = buffer.String()
 	fmt.Println(renderedOutput)
 }
