20e4ffa4dfaea4e2f5912c60c76954ae979a2572
Kunal Gautam <kunal@ikunal.in>
2021-10-19 09:58:24 +0530

    Day 34: More work on Utility.

--- 31-40/32-33-34/main.go
+++ 31-40/32-33-34/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/denisbrodbeck/machineid"
 	cpuUtil "github.com/shirou/gopsutil/v3/cpu"
@@ -21,29 +22,109 @@ func init() {
 	}
 }
 
+type swapM struct {
+	SwapTotal uint64
+	SwapUsed  uint64
+	SwapFree  uint64
+}
+
+type virtM struct {
+	VirtTotal     uint64
+	VirtAvailable uint64
+	VirtUsed      uint64
+	VirtFree      uint64
+}
+
+type DiskD struct {
+	DiskPath       string
+	DiskTotal      uint64
+	DiskUsed       uint64
+	DiskFree       uint64
+	DiskInodeTotal uint64
+	DiskInodeUsed  uint64
+	DiskInodeFree  uint64
+}
+
+type CPUData struct {
+	CpuCore  int32
+	CpuMHZ   float64
+	CpuCache int32
+	CpuCpu   int32
+}
+
+type LoadAvg struct {
+	LoadForOneMin     float64
+	LoadForFiveMin    float64
+	LoadForFifteenMin float64
+}
+
+type Data struct {
+	UniqID string
+	swapM
+	virtM
+	DiskD []DiskD
+	CPUData
+	LoadAvg
+}
+
 func main() {
-	fmt.Println(uniqMachId)
 	swapMemDetails, err := memUtility.SwapMemory()
 	errorHandling(err, "log")
-	fmt.Println(swapMemDetails)
+	swapData := swapM{
+		SwapTotal: swapMemDetails.Total,
+		SwapUsed:  swapMemDetails.Free,
+		SwapFree:  swapMemDetails.Free,
+	}
 
 	virtMemDetails, err := memUtility.VirtualMemory()
 	errorHandling(err, "log")
-	fmt.Println(virtMemDetails)
+	virtData := virtM{
+		VirtTotal:     virtMemDetails.Total,
+		VirtAvailable: virtMemDetails.Available,
+		VirtUsed:      virtMemDetails.Used,
+		VirtFree:      virtMemDetails.Free,
+	}
 
 	partitionDetails, err := diskUtil.Partitions(true)
 	errorHandling(err, "log")
-	fmt.Println(partitionDetails)
-	//To loop each partition to get details
-	//fmt.Println(diskUtil.Usage("/"))
+	var diskData []DiskD
+	for _, disk := range partitionDetails {
+		partDetails, err := diskUtil.Usage(disk.Mountpoint)
+		errorHandling(err, "log")
+		diskData = append(diskData, DiskD{
+			DiskPath:       partDetails.Path,
+			DiskTotal:      partDetails.Total,
+			DiskUsed:       partDetails.Used,
+			DiskFree:       partDetails.Free,
+			DiskInodeTotal: partDetails.InodesTotal,
+			DiskInodeUsed:  partDetails.InodesUsed,
+			DiskInodeFree:  partDetails.InodesFree,
+		})
+	}
 
 	cpuDetails, err := cpuUtil.Info()
 	errorHandling(err, "log")
-	fmt.Println(cpuDetails)
+	var cpuData CPUData
+	for _, cpu := range cpuDetails {
+		cpuData = CPUData{
+			CpuCore:  cpu.Cores,
+			CpuMHZ:   cpu.Mhz,
+			CpuCache: cpu.CacheSize,
+			CpuCpu:   cpu.CPU,
+		}
+
+	}
 
 	loadDetails, err := loadUtil.Avg()
 	errorHandling(err, "log")
-	fmt.Println(loadDetails)
+	loadData := LoadAvg{
+		LoadForOneMin:     loadDetails.Load1,
+		LoadForFiveMin:    loadDetails.Load5,
+		LoadForFifteenMin: loadDetails.Load15,
+	}
+	finalData, err := json.Marshal(Data{uniqMachId, swapData, virtData, diskData, cpuData, loadData})
+	errorHandling(err)
+	fmt.Println(string(finalData))
 }
 
 func errorHandling(err error, typeOfError ...string) {
